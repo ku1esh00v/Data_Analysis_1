a5f4c0d28f8074a88ced4544a888750bdebbd9d1
ku1esh00v <oleg.kuleshov.1261@mai.ru>
2024-02-27 12:36:22 +0300

    пример 15

---
+++ example_15.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+import sys
+
+
+if __name__ == "__main__":
+    print("Number of arguments:", len(sys.argv), "arguments")
+    print("Argument List:", str(sys.argv))
