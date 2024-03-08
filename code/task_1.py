2b826256058ddf9aebb1a99bd4b5ef9c981c5999
Oleg Kuleshov <144348564+ku1esh00v@users.noreply.github.com>
2024-03-08 23:55:35 +0300

    уменьшил время открытия файла

--- code/task_1.py
+++ code/task_1.py
@@ -3,23 +3,24 @@
 
 if __name__ == "__main__":
     with open('for_task_1.txt', 'r', encoding='utf-8') as file:
-        sentences_one_letter = []
-        other_sentences = []
+        content = file.read().splitlines()
 
-        for line in file:
-            sentence = line.strip()
-            words = sentence.split()
+    sentences_one_letter = []
+    other_sentences = []
 
-            if len(words) > 0:
-                if len(words[0]) == 1:
-                    sentences_one_letter.append(sentence)
-                else:
-                    other_sentences.append(sentence)
+    for sentence in content:
+        words = sentence.split()
 
-        print("Предложения, начинающиеся с одной буквы:")
-        for sentence in sentences_one_letter:
-            print(sentence)
+        if len(words) > 0:
+            if len(words[0]) == 1:
+                sentences_one_letter.append(sentence)
+            else:
+                other_sentences.append(sentence)
 
-        print("\nОстальные предложения:")
-        for sentence in other_sentences:
-            print(sentence)
+    print("Предложения, начинающиеся с одной буквы:")
+    for sentence in sentences_one_letter:
+        print(sentence)
+
+    print("\nОстальные предложения:")
+    for sentence in other_sentences:
+        print(sentence)
